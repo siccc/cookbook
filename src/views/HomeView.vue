903de9c3f47087718fca1c2dfb76c4d75a982a0c
Sarolta Nagy <nagysarolta124@gmail.com>
2022-10-17 15:49:17 +0200

    list recipe titles, testing vercel deploy

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
 import { reactive, onMounted } from 'vue';
 
+type Recipe = {
+  id: number,
+  title: string
+}
+
 const state = reactive({
-  recipes: null
+  recipes: [] as Recipe[]
 });
 onMounted(async () => {
   state.recipes = await (await fetch('/api/recipes')).json();
@@ -10,7 +15,10 @@ onMounted(async () => {
 </script>
 
 <template>
-  <main>
-    <div class="text-center p-4 text-2xl">Hello</div>
+  <main class="p-4">
+    <div class="text-center text-2xl">Recipes</div>
+    <div v-for="recipe in state.recipes" :key="recipe.id">
+      {{ recipe.title }} {{ recipe.id }}
+    </div>
   </main>
 </template>
